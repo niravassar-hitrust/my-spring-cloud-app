5a649d97ec4fa4b6c36c0e9405f1d8dfdb9ee5cd
nassar <assarn@objectcomputing.com>
2023-07-10 22:16:17 -0500

    user id

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
@@ -2,9 +2,13 @@ package com.appsdeveloperblog.photoapp.api.users.service;
 
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UsersService {
     @Override
     public UserDto createUser(UserDto userDto) {
+
+        userDto.setUserId(UUID.randomUUID().toString());
         return null;
     }
 }

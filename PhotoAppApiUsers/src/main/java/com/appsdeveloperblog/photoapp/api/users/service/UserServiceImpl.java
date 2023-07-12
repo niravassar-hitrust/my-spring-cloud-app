bbc2a5effa044d6c8f20697bf2c3d26edd05879a
nassar <assarn@objectcomputing.com>
2023-07-11 22:26:33 -0500

    create user with repository

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UserServiceImpl.java
@@ -1,14 +1,39 @@
 package com.appsdeveloperblog.photoapp.api.users.service;
 
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UsersService {
-    @Override
-    public UserDto createUser(UserDto userDto) {
 
-        userDto.setUserId(UUID.randomUUID().toString());
+    UsersRepository usersRepository;
+
+    @Autowired
+    public UserServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+
+        userDetails.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+
+        usersRepository.save(userEntity);
+
         return null;
     }
 }
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,7 +1,12 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +18,9 @@ public class UsersController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private UsersService usersService;
+
     @GetMapping("/status/check")
     public String status() {
         return "Working on port" + environment.getProperty("local.server.port");
@@ -20,6 +28,13 @@ public class UsersController {
 
     @PostMapping
     public String createUser(@RequestBody @Valid CreateUserRequestModel userDetails) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        usersService.createUser(userDto);
+
         return "Create user method is called";
     }
 }
